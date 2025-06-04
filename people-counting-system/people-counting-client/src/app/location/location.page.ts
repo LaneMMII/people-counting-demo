a717fe144d0638c30863b879f7c7a4b48bf6f081
Lane Simpson <lanesimpson14@gmail.com>
2025-06-04 19:43:59 -0400

    Updated location page UI to match wireframe, used device to create most of the code, updated location.page.ts to reflect necessary import additions as well as dummy data retrieved using postman and creating a observable.

--- people-counting-system/people-counting-client/src/app/location/location.page.ts
+++ people-counting-system/people-counting-client/src/app/location/location.page.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Observable, of } from 'rxjs';
+
+import { addIcons } from 'ionicons';
+import { addCircle, eyeOutline, createOutline, trashOutline } from 'ionicons/icons';
+
 import {
   IonContent,
   IonHeader,
@@ -8,6 +13,15 @@ import {
   IonToolbar,
   IonMenuButton,
   IonButtons,
+  IonButton,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonText
 } from '@ionic/angular/standalone';
 
 @Component({
@@ -16,18 +30,83 @@ import {
   styleUrls: ['./location.page.scss'],
   standalone: true,
   imports: [
-    IonContent,
-    IonHeader,
-    IonTitle,
-    IonToolbar,
-    CommonModule,
-    FormsModule,
-    IonMenuButton,
-    IonButtons,
-  ],
+      IonContent,
+      IonHeader,
+      IonTitle,
+      IonToolbar,
+      IonButton,
+      IonIcon,
+      IonGrid,
+      IonRow,
+      IonCol,
+      IonItem,
+      IonList,
+      IonLabel,
+      CommonModule,
+      FormsModule,
+      IonMenuButton,
+      IonButtons,
+      IonText
+    ],
 })
-export class LocationPage implements OnInit {
-  constructor() {}
 
-  ngOnInit() {}
+export class LocationPage implements OnInit {
+  locations$!: Observable<any[]>;
+
+  constructor() {
+    addIcons({ addCircle, eyeOutline, createOutline, trashOutline });
+  }
+
+  ngOnInit() {
+  this.locations$ = of([
+    {
+      id: 1,
+      name: "American Eagle",
+      address: "123 Mall St",
+      created: "2025-06-01T01:19:25.294Z",
+      updated: "2025-06-01T01:19:25.294Z",
+      deleted: null
+    },
+    {
+      id: 2,
+      name: "Woods Grocery",
+      address: "456 Market Ave",
+      created: "2025-06-01T01:19:25.294Z",
+      updated: "2025-06-01T01:19:25.294Z",
+      deleted: null
+    },
+    {
+      id: 3,
+      name: "Hot Topic",
+      address: "789 Fashion Blvd",
+      created: "2025-06-01T01:19:25.294Z",
+      updated: "2025-06-01T01:19:25.294Z",
+      deleted: null
+    },
+    {
+      id: 4,
+      name: "Game Stop",
+      address: "101 Gaming Ln",
+      created: "2025-06-01T01:19:25.294Z",
+      updated: "2025-06-01T01:19:25.294Z",
+      deleted: null
+    },
+    {
+      id: 5,
+      name: "Best Buy",
+      address: "202 Tech Rd",
+      created: "2025-06-01T01:19:25.294Z",
+      updated: "2025-06-01T01:19:25.294Z",
+      deleted: null
+    },
+    {
+      id: 6,
+      name: "Target",
+      address: "303 Retail Dr",
+      created: "2025-06-01T01:19:25.294Z",
+      updated: "2025-06-01T01:19:25.294Z",
+      deleted: null
+    }
+  ]);
+  }
 }
