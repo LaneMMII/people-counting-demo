a4ddc9de851365ec083a65c973cb410750aeaba4
Lane Simpson <lanesimpson14@gmail.com>
2025-06-05 00:06:25 -0400

    Implemented locaiton HTML for name and address and updated ts to reflect necessary imports.

--- people-counting-system/people-counting-client/src/app/add-location/add-location.page.ts
+++ people-counting-system/people-counting-client/src/app/add-location/add-location.page.ts
@@ -1,20 +1,96 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+
+import { addIcons } from 'ionicons';
+import {
+  addCircle,
+  eyeOutline,
+  createOutline,
+  trashOutline,
+  warningOutline
+} from 'ionicons/icons';
+
+import { 
+  IonContent, 
+  IonHeader, 
+  IonTitle, 
+  IonToolbar,
+  IonCardTitle,
+  IonInput,
+  IonSelect,
+  IonSelectOption,
+  IonCheckbox,
+  IonCardContent,
+  IonButton,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonItem,
+  IonList,
+  IonLabel,
+  IonMenuButton,
+  IonButtons,
+  IonText,
+  IonCard,
+  IonCardHeader,
+  IonNote
+  } from '@ionic/angular/standalone';
+import { add } from 'ionicons/icons';
 
 @Component({
   selector: 'app-add-location',
   templateUrl: './add-location.page.html',
   styleUrls: ['./add-location.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+      IonContent, 
+      IonCardTitle,
+      IonInput,
+      IonSelect,
+      IonSelectOption,
+      IonCheckbox,
+      IonHeader, 
+      IonTitle, 
+      IonToolbar, 
+      IonCardContent,
+      CommonModule, 
+      FormsModule,
+      IonButton,
+      IonIcon,
+      IonGrid,
+      IonRow,
+      IonCol,
+      IonItem,
+      IonList,
+      IonLabel,
+      IonMenuButton,
+      IonButtons,
+      IonText,
+      IonCard,
+      IonCardHeader,
+      IonNote]
 })
 export class AddLocationPage implements OnInit {
+  location = {name: '', address: ''}
 
-  constructor() { }
+  constructor() {  
+    addIcons({
+      addCircle,
+      eyeOutline,
+      createOutline,
+      trashOutline,
+      warningOutline
+    }); 
+  }
 
   ngOnInit() {
+  }
+
+  addLocation() {
+  //TODO: Implement the logic to add a new location
+  console.log("Add Location button clicked");
   }
 
 }
