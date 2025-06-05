67ee3ffbfa6e66aafc4ac1b71bd54a144841f30a
Lane Simpson <lanesimpson14@gmail.com>
2025-06-05 16:57:47 -0400

    Added dummy data for prefill

--- people-counting-system/people-counting-client/src/app/edit-device/edit-device.page.ts
+++ people-counting-system/people-counting-client/src/app/edit-device/edit-device.page.ts
@@ -74,9 +74,104 @@ import { ActivatedRoute } from '@angular/router';
   IonSelect,
   IonCheckbox]
 })
+
 export class EditDevicePage implements OnInit {
-  device = { name: '', location: '', active: false };
-  locations$!: Observable<any[]>; 
+  device = { name: '', location: 0, active: false };
+  locations$!: Observable<any[]>;
+
+  // Mock devices and locations need updated to use actual data from db
+  private devices = [
+    {
+      id: 1,
+      name: 'American Eagle East Door',
+      locationId: 1,
+      active: true,
+      created: '2025-06-01T01:19:25.296Z',
+      updated: '2025-06-01T01:19:25.296Z',
+      deleted: null,
+    },
+    {
+      id: 2,
+      name: 'American Eagle West Door',
+      locationId: 1,
+      active: true,
+      created: '2025-06-01T01:19:25.296Z',
+      updated: '2025-06-01T01:19:25.296Z',
+      deleted: null,
+    },
+    {
+      id: 3,
+      name: 'Woods Grocery North Door',
+      locationId: 2,
+      active: true,
+      created: '2025-06-01T01:19:25.296Z',
+      updated: '2025-06-01T01:19:25.296Z',
+      deleted: null,
+    },
+    {
+      id: 4,
+      name: 'Woods Grocery South Door',
+      locationId: 2,
+      active: true,
+      created: '2025-06-01T01:19:25.296Z',
+      updated: '2025-06-01T01:19:25.296Z',
+      deleted: null,
+    },
+    {
+      id: 5,
+      name: 'Hot Topic',
+      locationId: 3,
+      active: true,
+      created: '2025-06-01T01:19:25.296Z',
+      updated: '2025-06-01T01:19:25.296Z',
+      deleted: null,
+    },
+    {
+      id: 6,
+      name: 'Game Stop',
+      locationId: 4,
+      active: true,
+      created: '2025-06-01T01:19:25.296Z',
+      updated: '2025-06-01T01:19:25.296Z',
+      deleted: null,
+    },
+    {
+      id: 7,
+      name: 'Best Buy East Door',
+      locationId: 5,
+      active: true,
+      created: '2025-06-01T01:19:25.296Z',
+      updated: '2025-06-01T01:19:25.296Z',
+      deleted: null,
+    },
+    {
+      id: 8,
+      name: 'Best Buy West Door',
+      locationId: 5,
+      active: true,
+      created: '2025-06-01T01:19:25.296Z',
+      updated: '2025-06-01T01:19:25.296Z',
+      deleted: null,
+    },
+    {
+      id: 9,
+      name: 'Target East Door',
+      locationId: 6,
+      active: true,
+      created: '2025-06-01T01:19:25.296Z',
+      updated: '2025-06-01T01:19:25.296Z',
+      deleted: null,
+    },
+    {
+      id: 10,
+      name: 'Target West Door',
+      locationId: 6,
+      active: true,
+      created: '2025-06-01T01:19:25.296Z',
+      updated: '2025-06-01T01:19:25.296Z',
+      deleted: null,
+    },
+  ];
 
   constructor(private route: ActivatedRoute) {
     addIcons({
@@ -90,65 +185,27 @@ export class EditDevicePage implements OnInit {
 
   ngOnInit() {
     this.locations$ = of([
-          {
-            id: 1,
-            name: 'American Eagle',
-            address: '123 Mall St',
-            created: '2025-06-01T01:19:25.294Z',
-            updated: '2025-06-01T01:19:25.294Z',
-            deleted: null,
-          },
-          {
-            id: 2,
-            name: 'Woods Grocery',
-            address: '456 Market Ave',
-            created: '2025-06-01T01:19:25.294Z',
-            updated: '2025-06-01T01:19:25.294Z',
-            deleted: null,
-          },
-          {
-            id: 3,
-            name: 'Hot Topic',
-            address: '789 Fashion Blvd',
-            created: '2025-06-01T01:19:25.294Z',
-            updated: '2025-06-01T01:19:25.294Z',
-            deleted: null,
-          },
-          {
-            id: 4,
-            name: 'Game Stop',
-            address: '101 Gaming Ln',
-            created: '2025-06-01T01:19:25.294Z',
-            updated: '2025-06-01T01:19:25.294Z',
-            deleted: null,
-          },
-          {
-            id: 5,
-            name: 'Best Buy',
-            address: '202 Tech Rd',
-            created: '2025-06-01T01:19:25.294Z',
-            updated: '2025-06-01T01:19:25.294Z',
-            deleted: null,
-          },
-          {
-            id: 6,
-            name: 'Target',
-            address: '303 Retail Dr',
-            created: '2025-06-01T01:19:25.294Z',
-            updated: '2025-06-01T01:19:25.294Z',
-            deleted: null,
-          },
-        ]);
+      { id: 1, name: 'American Eagle', address: '123 Mall St' },
+      { id: 2, name: 'Woods Grocery', address: '456 Market Ave' },
+      { id: 3, name: 'Hot Topic', address: '789 Fashion Blvd' },
+      { id: 4, name: 'Game Stop', address: '101 Gaming Ln' },
+      { id: 5, name: 'Best Buy', address: '202 Tech Rd' },
+      { id: 6, name: 'Target', address: '303 Retail Dr' },
+    ]);
 
-        const id = this.route.snapshot.paramMap.get('id');
-        if (id) {
-          // Logic to fetch the device details by ID
-          
-        }
+const id = Number(this.route.snapshot.paramMap.get('id'));
+    const found = this.devices.find(dev => dev.id === id);
+    if (found) {
+      this.device = {
+        name: found.name,
+        location: found.locationId,
+        active: found.active
+      };
+    }
   }
 
   updateDevice() {
     // Logic to update the device
-    console.log('Device updated');
+    console.log('Device updated', this.device);
   }
 }
--- people-counting-system/people-counting-client/src/app/edit-location/edit-location.page.ts
+++ people-counting-system/people-counting-client/src/app/edit-location/edit-location.page.ts
@@ -1,31 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { 
-  IonContent, 
-  IonHeader, 
-  IonTitle, 
-  IonToolbar,
-  IonButton,
-  IonIcon,
-  IonMenuButton,
-  IonButtons,
-  IonGrid,
-  IonRow,
-  IonCol,
-  IonList,
-  IonLabel,
-  IonItem,
-  IonText,
-  IonNote,
-  IonCardHeader,
-  IonCardContent,
-  IonCardTitle,
-  IonCard,
-  IonInput,
-  IonSelectOption,
-  IonSelect,
-  IonCheckbox} from '@ionic/angular/standalone';
 
 import { addIcons } from 'ionicons';
 import {
@@ -36,57 +12,94 @@ import {
   warningOutline
 } from 'ionicons/icons';
 
+import { 
+  IonContent, 
+  IonHeader, 
+  IonTitle, 
+  IonToolbar,
+  IonCardTitle,
+  IonInput,
+  IonCardContent,
+  IonButton,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonItem,
+  IonList,
+  IonLabel,
+  IonMenuButton,
+  IonButtons,
+  IonText,
+  IonCard,
+  IonCardHeader,
+  IonNote
+} from '@ionic/angular/standalone';
+
 @Component({
   selector: 'app-edit-location',
   templateUrl: './edit-location.page.html',
   styleUrls: ['./edit-location.page.scss'],
   standalone: true,
-  imports: [ 
-    CommonModule,
+  imports: [
+    IonContent, 
+    IonCardTitle,
+    IonInput,
+    IonHeader, 
+    IonTitle, 
+    IonToolbar, 
+    IonCardContent,
+    CommonModule, 
     FormsModule,
-    IonContent,
-    IonHeader,
-    IonTitle,
-    IonToolbar,
     IonButton,
     IonIcon,
-    IonMenuButton,
-    IonButtons,
     IonGrid,
     IonRow,
     IonCol,
+    IonItem,
     IonList,
     IonLabel,
-    IonItem,
+    IonMenuButton,
+    IonButtons,
     IonText,
-    IonNote,
-    IonCardHeader,
-    IonCardContent,
-    IonCardTitle,
     IonCard,
-    IonInput,
-    IonSelectOption,
-    IonSelect,
-    IonCheckbox]
+    IonCardHeader,
+    IonNote
+  ]
 })
 export class EditLocationPage implements OnInit {
-    location = {name: '', address: ''}
+  location = { name: '', address: '' };
 
-  constructor() {
+  // Mock locations array (replace with service in real app)
+  private locations = [
+    { id: 1, name: 'American Eagle', address: '123 Mall St' },
+    { id: 2, name: 'Woods Grocery', address: '456 Market Ave' },
+    { id: 3, name: 'Hot Topic', address: '789 Fashion Blvd' },
+    { id: 4, name: 'Game Stop', address: '101 Gaming Ln' },
+    { id: 5, name: 'Best Buy', address: '202 Tech Rd' },
+    { id: 6, name: 'Target', address: '303 Retail Dr' },
+  ];
+
+  constructor(private route: ActivatedRoute) {  
     addIcons({
       addCircle,
       eyeOutline,
       createOutline,
       trashOutline,
       warningOutline
-    });
-   }
+    }); 
+  }
 
   ngOnInit() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    const found = this.locations.find(loc => loc.id === id);
+    if (found) {
+      this.location = { ...found };
+    }
   }
 
   updateLocation() {
-    // Logic to update the location
-    console.log('Location updated');
+    // TODO: Implement the logic to update the location
+    console.log("Location updated", this.location);
   }
 }
