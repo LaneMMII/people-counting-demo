4fe3897d073cb6ebf42039f9732a4529b5eb7a40
Lane Simpson <lanesimpson14@gmail.com>
2025-06-09 16:19:14 -0400

    Started device services

--- people-counting-system/people-counting-client/src/app/device/device.page.ts
+++ people-counting-system/people-counting-client/src/app/device/device.page.ts
@@ -30,6 +30,7 @@ import {
 } from 'ionicons/icons';
 
 import { Observable, of } from 'rxjs';
+import { DeviceService, Device } from '../services/device.service';
 
 @Component({
   selector: 'app-device',
@@ -58,9 +59,9 @@ import { Observable, of } from 'rxjs';
   ],
 })
 export class DevicePage implements OnInit {
-  devices$!: Observable<any[]>;
+  devices$!: Observable<Device[]>;
 
-  constructor() {
+  constructor(private deviceService: DeviceService) {
     addIcons({
       addCircle,
       eyeOutline,
@@ -71,97 +72,6 @@ export class DevicePage implements OnInit {
   }
 
   ngOnInit() {
-    this.devices$ = of([
-      {
-        id: 1,
-        name: 'American Eagle East Door',
-        locationId: 1,
-        active: true,
-        created: '2025-06-01T01:19:25.296Z',
-        updated: '2025-06-01T01:19:25.296Z',
-        deleted: null,
-      },
-      {
-        id: 2,
-        name: 'American Eagle West Door',
-        locationId: 1,
-        active: true,
-        created: '2025-06-01T01:19:25.296Z',
-        updated: '2025-06-01T01:19:25.296Z',
-        deleted: null,
-      },
-      {
-        id: 3,
-        name: 'Woods Grocery North Door',
-        locationId: 2,
-        active: true,
-        created: '2025-06-01T01:19:25.296Z',
-        updated: '2025-06-01T01:19:25.296Z',
-        deleted: null,
-      },
-      {
-        id: 4,
-        name: 'Woods Grocery South Door',
-        locationId: 2,
-        active: true,
-        created: '2025-06-01T01:19:25.296Z',
-        updated: '2025-06-01T01:19:25.296Z',
-        deleted: null,
-      },
-      {
-        id: 5,
-        name: 'Hot Topic',
-        locationId: 3,
-        active: true,
-        created: '2025-06-01T01:19:25.296Z',
-        updated: '2025-06-01T01:19:25.296Z',
-        deleted: null,
-      },
-      {
-        id: 6,
-        name: 'Game Stop',
-        locationId: 4,
-        active: true,
-        created: '2025-06-01T01:19:25.296Z',
-        updated: '2025-06-01T01:19:25.296Z',
-        deleted: null,
-      },
-      {
-        id: 7,
-        name: 'Best Buy East Door',
-        locationId: 5,
-        active: true,
-        created: '2025-06-01T01:19:25.296Z',
-        updated: '2025-06-01T01:19:25.296Z',
-        deleted: null,
-      },
-      {
-        id: 8,
-        name: 'Best Buy West Door',
-        locationId: 5,
-        active: true,
-        created: '2025-06-01T01:19:25.296Z',
-        updated: '2025-06-01T01:19:25.296Z',
-        deleted: null,
-      },
-      {
-        id: 9,
-        name: 'Target East Door',
-        locationId: 6,
-        active: true,
-        created: '2025-06-01T01:19:25.296Z',
-        updated: '2025-06-01T01:19:25.296Z',
-        deleted: null,
-      },
-      {
-        id: 10,
-        name: 'Target West Door',
-        locationId: 6,
-        active: true,
-        created: '2025-06-01T01:19:25.296Z',
-        updated: '2025-06-01T01:19:25.296Z',
-        deleted: null,
-      },
-    ]);
+    this.devices$ = this.deviceService.getDevices();
   }
 }
---
+++ people-counting-system/people-counting-client/src/app/services/device.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
+
+export interface Device {
+  id?: number;
+  name: string;
+  locationId: number;
+  active: boolean;
+  created?: string;
+  updated?: string;
+  deleted?: string | null;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DeviceService {
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getDevices(): Observable<Device[]> {
+    return this.http.get<{ devices: Device[] }>(`${this.apiUrl}/device`).pipe(
+      map(res => res.devices),
+      catchError(err => throwError(() => err))
+    );
+  }
+
+  getDevice(id: number): Observable<Device> {
+    return this.http.get<{ device: Device }>(`${this.apiUrl}/device/${id}`).pipe(
+      map(res => res.device),
+      catchError(err => throwError(() => err))
+    );
+  }
+
+  createDevice(device: Device): Observable<Device> {
+    return this.http.post<{ device: Device }>(`${this.apiUrl}/device`, device).pipe(
+      map(res => res.device),
+      catchError(err => throwError(() => err))
+    );
+  }
+
+  updateDevice(id: number, device: Device): Observable<Device> {
+    return this.http.put<{ device: Device }>(`${this.apiUrl}/device/${id}`, device).pipe(
+      map(res => res.device),
+      catchError(err => throwError(() => err))
+    );
+  }
+
+  deleteDevice(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/device/${id}`).pipe(
+      catchError(err => throwError(() => err))
+    );
+  }
+}
